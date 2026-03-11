d0e78b3da40d63a2a1f49707ccb5ecf0a65deceb
leonardo.pereira <leonardo.pereira@wdhouse.com.br>
2026-03-11 16:25:41 -0300

    feat: adicionada funções para data e filtros para a request

--- server/rss/server.js
+++ server/rss/server.js
@@ -25,36 +25,82 @@ async function decodeGoogleNewsUrl(googleUrl) {
     }
 }
 
+const ALLOWED_SITES = [
+    'br.ign.com',
+    'theenemy.com.br',
+    'gamevicio.com',
+    'flowgames.gg',
+    'manualdosgames.com',
+    'gamehall.com.br',
+    'nintendoblast.com.br',
+    'psxbrasil.com.br',
+    'centralxbox.com.br',
+    'techtudo.com.br/jogos',
+    'canaltech.com.br/games',
+    'uol.com.br/start',
+    'terra.com.br/gameon',
+    'g1.globo.com/pop-arte/games',
+    'tecmundo.com.br/voxel',
+    'adrenaline.com.br'
+];
+
+const BLOCKED_SITES = [
+    'pinterest.com',
+    'mercadolivre.com.br',
+    'cesan.com.br'
+];
+
+const BLOCKED_TERMS = [
+    'criptomoeda',
+    'loteria',
+    'aposta',
+    'cassino'
+];
+
+function buildSearchQuery() {
+    const gameTerms = [
+        '"jogos de video game"',
+        '"videogame"',
+        '"video game"',
+        '"games"',
+        '"jogos eletrônicos"'
+    ].join(' OR\n');
+
+    const siteFilters = ALLOWED_SITES
+        .map(site => `site:${site}`)
+        .join(' OR\n');
+
+    const blockedSites = BLOCKED_SITES
+        .map(site => `-site:${site}`)
+        .join('\n');
+
+    const blockedTerms = BLOCKED_TERMS
+        .map(term => `-${term}`)
+        .join('\n');
+
+    return `
+(
+${gameTerms}
+)
+(
+${siteFilters}
+)
+${blockedSites}
+${blockedTerms}
+`;
+}
 
 export async function searchNews(termo) {
     try {
-        const query = `
-(
-"jogos de video game" OR
-"videogame" OR
-"video game" OR
-"games" OR
-"jogos eletrônicos"
-)
-(
-site:tecmundo.com.br OR
-site:canaltech.com.br OR
-site:adrenaline.com.br OR
-site:voxel.com.br
-)
--site:pinterest.com
--site:mercadolivre.com.br
--cesan.com.br
--criptomoeda 
--loteria
- -aposta 
- -cassino
-`;
+        const query = buildSearchQuery();
         const url = `https://news.google.com/rss/search?q=${encodeURIComponent(query)}&hl=pt-BR&gl=BR&ceid=BR:pt-419`;
         const feed = await parser.parseURL(url);
-        const totalItems = feed.items.length
+
+        const now = new Date();
+        const thirtyDaysAgo = new Date(now.getTime() - (365 * 24 * 60 * 60 * 1000));
+
         const articles = await Promise.all(
-            feed.items.slice(0, 100).map(async item => {
+            feed.items.slice(0, 500).map(async item => {
                 const decoded = await decodeGoogleNewsUrl(item.link);
                 return {
                     title: item.title,
@@ -65,16 +111,21 @@ site:voxel.com.br
                 };
             })
         );
+
+        const filteredArticles = articles
+            .filter(article => new Date(article.pubDate) >= thirtyDaysAgo)
+            .sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+
         return {
-            totalItems,
-            articles: articles.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate))
-        }
-        console.log('Dados coletados com sucesso')
+            totalItems: filteredArticles.length,
+            articles: filteredArticles
+        };
     } catch (error) {
         console.error("Erro:", error);
-        return [];
+        return { totalItems: 0, articles: [] };
     }
 }
+
 // (async () => {
 //     const noticias = await searchNews();
 //     fs.writeFileSync('gameNews.json', JSON.stringify(noticias, null, 2))
